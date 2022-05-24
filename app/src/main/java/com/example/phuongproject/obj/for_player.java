7a546c8b19bc3cb5b63871456d919442d8a0815c
RouiePLNT <18521272@gm.uit.edu.vn>
2022-05-24 21:23:20 +0700

    Update for_player.java

--- app/src/main/java/com/example/phuongproject/obj/for_player.java
+++ app/src/main/java/com/example/phuongproject/obj/for_player.java
@@ -4,18 +4,18 @@ import android.content.Context;
 import android.content.SharedPreferences;
 
 public class for_player {
-    private String nameData = "appData";
+    private String sharePref = "appData";
     public int money;
     public void saveInformation(Context ct)
     {
-        SharedPreferences settings = ct.getSharedPreferences(nameData, 0);
+        SharedPreferences settings = ct.getSharedPreferences(sharePref, 0);
         SharedPreferences.Editor editor = settings.edit();
         editor.putInt("money", money );
         editor.commit();
     }
     public void getInformation(Context ct)
     {
-        SharedPreferences settings = ct.getSharedPreferences(nameData, 0);
+        SharedPreferences settings = ct.getSharedPreferences(sharePref, 0);
         money = settings.getInt("money", 10);
 
     }
